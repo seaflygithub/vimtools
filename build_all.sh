065fe12d9b79f0c11fbf4eafa074c47f2c914d96
SeaflyDennis <seafly0616@qq.com>
2017-04-30 16:45:25 +0800

    最后一次更新

--- build_all.sh
+++ build_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # build_all.sh
 # Copyright (c) 2017 SeaflyDennis <seafly0616@qq.com>
-#
+# 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
@@ -14,9 +14,6 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#---------------------------------------
-#(configuration item)
-#---------------------------------------------
 CUR=
 MAKE=make
 MKCLEAN=distclean
